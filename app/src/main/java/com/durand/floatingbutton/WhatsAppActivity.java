759f369299276e91ba40440fd30dd827bb6770bd
Josue Durand <tismart@JosueDurand.local>
2020-05-22 02:42:32 -0500

    wsp

--- app/src/main/java/com/durand/floatingbutton/WhatsAppActivity.java
+++ app/src/main/java/com/durand/floatingbutton/WhatsAppActivity.java
@@ -3,10 +3,12 @@ package com.durand.floatingbutton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -24,18 +26,19 @@ public class WhatsAppActivity extends AppCompatActivity {
         setContentView(R.layout.activity_whats_app);
 
         final EditText editText_mobile = findViewById(R.id.editText_mobile);
-        final EditText editText_msg =  findViewById(R.id.editText_msg);
+        final EditText editText_msg = findViewById(R.id.editText_msg);
 
         btnWsp = findViewById(R.id.btnWsp);
         btnWsp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean installed = appInstalledOrNot("com.whatsapp");
 
-                if (installed){
+                boolean isWhatsappInstalled = whatsappInstalledOrNot("com.whatsapp");
+                if (isWhatsappInstalled) {
+                    Toast.makeText(getApplicationContext(), "Installed", Toast.LENGTH_SHORT).show();
                     String mobileNumber = editText_mobile.getText().toString();
                     String message = editText_msg.getText().toString();
-                    String toNumber = "+51"+mobileNumber; // contains spaces.
+                    String toNumber = "+51" + mobileNumber; // contains spaces.
                     toNumber = toNumber.replace("+", "").replace(" ", "");
                     Intent sendIntent = new Intent("android.intent.action.MAIN");
                     sendIntent.putExtra("jid", toNumber + "@s.whatsapp.net");
@@ -44,26 +47,46 @@ public class WhatsAppActivity extends AppCompatActivity {
                     sendIntent.setPackage("com.whatsapp");
                     sendIntent.setType("text/plain");
                     startActivity(sendIntent);
-
-                }else{
-                    Toast.makeText(WhatsAppActivity.this, "Instala whastapp amiguito", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(WhatsAppActivity.this, " Si tiene wsp yupii  ", Toast.LENGTH_SHORT).show();
+                } else {
+                    Uri uri = Uri.parse("market://details?id=com.whatsapp");
+                    Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                    Toast.makeText(getApplicationContext(), "Instala wsp amiguito", Toast.LENGTH_SHORT).show();
+                    startActivity(goToMarket);
                 }
+
+
+                // boolean installed = appInstalledOrNot("com.whatsapp");
+                //  if (installed){
+//                    String mobileNumber = editText_mobile.getText().toString();
+//                    String message = editText_msg.getText().toString();
+//                    String toNumber = "+51"+mobileNumber; // contains spaces.
+//                    toNumber = toNumber.replace("+", "").replace(" ", "");
+//                    Intent sendIntent = new Intent("android.intent.action.MAIN");
+//                    sendIntent.putExtra("jid", toNumber + "@s.whatsapp.net");
+//                    sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+//                    sendIntent.setAction(Intent.ACTION_SEND);
+//                    sendIntent.setPackage("com.whatsapp");
+//                    sendIntent.setType("text/plain");
+//                    startActivity(sendIntent);
+                //   Toast.makeText(WhatsAppActivity.this, " whastapp ", Toast.LENGTH_SHORT).show();
+                // }else{
+                //   Toast.makeText(WhatsAppActivity.this, "Instala whastapp amiguito", Toast.LENGTH_SHORT).show();
+                // }
 
             }
         });
     }
 
-    private boolean appInstalledOrNot(String url){
-        PackageManager packageManager =  getPackageManager();
+    private boolean whatsappInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
         boolean app_installed;
         try {
-            packageManager.getPackageInfo(url,PackageManager.GET_ACTIVITIES);
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
             app_installed = true;
-
-        }catch (PackageManager.NameNotFoundException e){
+        } catch (PackageManager.NameNotFoundException e) {
             app_installed = false;
         }
-        return  app_installed;
+        return app_installed;
     }
-
 }
