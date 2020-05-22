7f33a011e7aa3f6d507b808e427c676393ecf8c0
Josue Durand <tismart@JosueDurand.local>
2020-05-22 02:48:25 -0500

    open web

--- app/src/main/java/com/durand/floatingbutton/WebActivity.java
+++ app/src/main/java/com/durand/floatingbutton/WebActivity.java
@@ -1,14 +1,30 @@
 package com.durand.floatingbutton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WebActivity extends AppCompatActivity {
 
+    ConstraintLayout btnWebex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        btnWebex = findViewById(R.id.btnWebex);
+        btnWebex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://rokys.com/pedido_rokys/index2.html"));
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
