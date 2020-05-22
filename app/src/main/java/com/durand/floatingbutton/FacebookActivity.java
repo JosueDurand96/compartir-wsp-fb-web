a0e41dc3be3fe9dd197e988fbd89213a9e742c16
Josue Durand <tismart@JosueDurand.local>
2020-05-22 03:16:00 -0500

    face

--- app/src/main/java/com/durand/floatingbutton/FacebookActivity.java
+++ app/src/main/java/com/durand/floatingbutton/FacebookActivity.java
@@ -1,14 +1,76 @@
 package com.durand.floatingbutton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
 
 public class FacebookActivity extends AppCompatActivity {
-
+    ConstraintLayout btnFb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook);
+        btnFb = findViewById(R.id.btnFb);
+        btnFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //0
+                launchFacebook();
+
+
+                //1
+//                String YourPageURL = "https://www.facebook.com/manyar.comer";
+//                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(YourPageURL));
+//                startActivity(browserIntent);
+
+
+//                try {
+//                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/" + pageId));
+//                } catch (Exception e) {
+//                    intent =  new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/" + pageId));
+//                }
+
+
+
+
+
+                //2
+//                try {
+//                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/manyar.comer"));
+//                    startActivity(intent);
+//                } catch(Exception e) {
+//                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/manyar.comer")));
+//                }
+            }
+        });
+    }
+
+
+    public final void launchFacebook() {
+        final String urlFb = "fb://page/"+"https://www.facebook.com/manyar.comer";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(urlFb));
+
+        // If a Facebook app is installed, use it. Otherwise, launch
+        // a browser
+        final PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        if (list.size() == 0) {
+           // final String urlBrowser = "https://www.facebook.com/"+pageid;
+            final String urlBrowser = "https://www.facebook.com/manyar.comer";
+            intent.setData(Uri.parse(urlBrowser));
+        }
+
+        startActivity(intent);
     }
 }
